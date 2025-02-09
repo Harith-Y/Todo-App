546c3c95a1f8b0f0fd7ddab4b56268926773ec4f
Harith Yerragolam <yharith16@gmail.com>
2025-02-09 17:11:12 +0530

    Add Bootstrap for styling and restructure app layout; remove HelloWorld component

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,11 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="app-container row justify-content-center mt-3">
+    <div class="col-md-8 border">
+      <router-view/>
+    </div>
+    
+  </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/components/NewTaskFormAndTasksList.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="newTaskFormAndTasksList">
+        <NewTaskForm />
+        <TasksLists />
+    </div>
+</template>
+
+<script>
+
+    import NewTaskForm from './components/NewTaskForm.vue';
+    import TasksList from './components/TasksList.vue';
+    
+    export default {
+        name: "App",
+        components: {
+            NewTaskForm,
+            TasksList
+        },
+        data() {
+            return {
+                tasks: [],
+                newTask: ''
+            }
+        },
+        methods: {
+            addTask() {
+                this.tasks.push(this.newTask);
+                this.newTask = '';
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
