956394ebd76861bedf829e6a1ce11ba942e42506
Harith Yerragolam <yharith16@gmail.com>
2025-02-11 18:57:55 +0530

    Enhance TasksList component with a structured table layout for task display; add edit and delete functionality

--- src/components/TasksList.vue
+++ src/components/TasksList.vue
@@ -1,6 +1,26 @@
 <template>
-    <div>
+    <div class = 'TaskList'>
         <h2> List of Tasks go here</h2>
+        <table key="table">
+            <thead class="thead-dark">
+                <tr>
+                    <th class="description">Description</th>
+                    <th class="edit">Edit</th>
+                    <th class="delete">Delete</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="{ id, description, completed } in tasks" :key="id">
+                    <td class="description" v-bind:class="{completed: completed}">{{ description }}</td>
+                    <td> 
+                        <router-link :to="'/editTask?taskID=' + id" class="btn btn-primary">
+                            Edit
+                        </router-link>
+                    </td>
+                    <td><button class="btn btn-danger">Delete</button></td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
@@ -11,5 +31,12 @@
 </script>
 
 <style>
+.tasksList table th.edit,
+.tasksList table th.delete {
+    width:100px;
+}
 
+.completed {
+    text-decoration: line-through;
+}
 </style>
